3e2db43b7df06ba0b3ad5452fd48317805756cf2
NewsNing <newsning@sina.com>
2016-11-27 00:30:05 +0800

    优化 跨页面 数据双向绑定

--- js/App/Vhp.js
+++ js/App/Vhp.js
@@ -1,67 +1,105 @@
 (function($) {
 	var vhp = function(o) {
-			this.ops = (function(_o, _v) {
-				for(var i in o) {
+			//object 深度合并
+			this.ops = (function _extend(_v, _o) {
+				var has;
+				for(var i in _o) {
+
+					has = false;
+					for(var j in _v[i]) {
+						has = true;
+					}
+					if(has && typeof _o[i] === 'object') {
+						_o[i] = _extend(_v[i], _o[i]);
+					}
 					_v[i] = _o[i];
 				}
 				return _v;
-			}(o, {
+			}({
 				el: '#app',
-				watch:{}
-			}));
+				data: {
+					vhpstate$: 'self$'
+				},
+				watch: {},
+				listens: ['isother$']
+			}, o));
 			this.init();
 		},
 		pro = vhp.prototype;
-
+		
+	//初始化自定义监听列表
 	pro._init_listens = function() {
-		var arr = this.ops.listens,temp,that = this;
+		var arr = this.ops.listens,
+			temp, that = this;
 		for(var i in arr) {
 			temp = arr[i];
 			if(!this.ops.watch[temp]) {
-				this.ops.watch[temp] = function(val){
-					that._sendAllMessage(temp,val);
+				this.ops.watch[temp] = function(val) {
+					//判断是否是自身引起的数据变化
+					if(that.vm.vhpstate$ === 'self$') {
+						//通知
+						that._sendAllMessage(temp, val);
+					}
 				}
 			}
 		}
+
 	}
 	
-	pro._init_reg = function(){
+	//注册数据变化事件
+	pro._init_reg = function() {
 		var that = this;
-		window.addEventListener('_VHP_WATCH_CHANGE_',function(data){
+		window.addEventListener('_VHP_WATCH_CHANGE_', function(data) {
 			data = data.detail.data;
-			for(var i in data){
-				that.vm[i] = data[i]; 
+			
+			//状态改为 是其
+			that.vm['vhpstate$'] = 'other$';
+
+			//数据赋值   待优化
+			for(var i in data) {
+				that.vm[i] = data[i];
 			}
+			
+			//状态改为 自身
+			setTimeout(function() {
+				that.vm['vhpstate$'] = 'self$';
+			}, 25);
+
 		});
 	}
 	
-	pro._sendAllMessage = function(k,v){
-		var that = this,data={};
+	//发送数据变化通知
+	pro._sendAllMessage = function(k, v) {
+		var that = this,
+			data = {},
+			temp;
 		data[k] = v;
-		$.plusReady(function(){
-			for(var i in that.webviews){
-				$.fire(that.webviews[i],'_VHP_WATCH_CHANGE_',{
-					data:data
+		$.plusReady(function() {
+			for(var i in that.webviews) {
+				temp = that.webviews[i];
+				$.fire(temp, '_VHP_WATCH_CHANGE_', {
+					data: data
 				});
-				
+
 			}
 		});
 	}
 
 	pro.init = function() {
 		var that = this;
-		
+
 		this._init_listens();
-		
+
 		this._init_reg();
-		
+
 		this.vm = new Vue(this.ops);
-		
-		$.plusReady(function(){
+
+		$.plusReady(function() {
+			//隐性bug?
 			that.webviews = plus.webview.all();
 		});
 	}
-	
+
 	window.VHP = vhp;
 
 }(mui));
