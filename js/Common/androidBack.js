625fbd584c78eac3bb0b166cbbbad01a06641e4f
NewsNing <newsning@sina.com>
2016-12-09 18:20:03 +0800

    优化安卓双击后台

--- js/Common/androidBack.js
+++ js/Common/androidBack.js
@@ -1,20 +1,19 @@
 (function($) {
-	var backButtonPress = 0,main;
-	//小细节，自动获取main先
-	$.plusReady(getMain);
-	function getMain(){
+	
+	var main,
+		backButtonPress = 0;
+		
+	$.plusReady(function(){
 		main =  plus.android.runtimeMainActivity();
-	}
+	});
+	
 	$.back = function() {
 		backButtonPress++;
 		if (backButtonPress > 1) {
 			//退出
 			//plus.runtime.quit();
 			//后台
-			if(!main){
-				getMain();
-			}
-			main.moveTaskToBack(false);
+			main && main.moveTaskToBack(false);
 		} else {
 			plus.nativeUI.toast('再按一次返回桌面');
 		}
@@ -23,4 +22,5 @@
 		}, 1000);
 		return false;
 	};
+	
 }(mui));
